d0862a0b7d5d1b38b3f7bf2c7831bb6239bdfbd4
Thaisllayne <thaisllaynesantana@gmail.com>
2021-07-12 20:58:16 -0300

    Ideia principal finalizada

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
-const verificarNomePokemon = async (infoNecessaria) => {
-      //nome do pokemon na API:
-      var nomeNaAPI = infoNecessaria.name;
-      console.log("o nome do pokemon é: " + nomeNaAPI);
-
-
-/*       console.log(infoNecessaria.name)
-      if (nomePokemon != infoNecessaria.name) {
-            console.log('nome ta errado')
-            document.querySelector("#erro").innerHTML = 'Nome incorreto'
-      } */
-};
-
-const conectarAPI = async () => {
-      // buscando o valor do HTML - pelo nome:
-      let nomePokemon = document.querySelector("#nomePokemon").value.toLowerCase();
-      let idPokemon = document.querySelector("#idPokemon").value;
-
-      if (nomePokemon == "" && idPokemon == "") {
-            document.querySelector("#erro").innerHTML = 'Preencha um dos campos';
-      }
-
-      if (nomePokemon == "") {
-            var url = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
-      }
-      if (idPokemon == '') {
-            var url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
-            verificarNomePokemon(infoNecessaria);
-      }
-      var dados = await fetch(url);
-      var infoNecessaria = await dados.json();
-
-
-
+const habilidadesPokemon = (infoNecessaria) =>{
       // Habilidades do Pokémon:
       var habilidades = infoNecessaria.stats;
 
@@ -47,30 +14,65 @@ const conectarAPI = async () => {
       document.querySelector("#defense").innerHTML = defense;
       document.querySelector("#specialAttack").innerHTML = specialAttack;
       document.querySelector("#specialDefense").innerHTML = specialDefense;
-      document.querySelector("#speed").innerHTML = speed;
+      document.querySelector("#speed").innerHTML = speed;      
+}
 
-/*    for (let i = 0; i < habilidades.length; i++) {
+/*  const verificarNomePokemon = async (nomePokemonAPI) => {
+      //nome do pokemon na API:
+      console.log(nomePokemonAPI)
 
-            //var valor = Number(habilidades.base_stat);
-            //console.log(valor)
+      if (nomePokemon != nomePokemonAPI) {
+            console.log('nome ta errado')
+            document.querySelector("#erro").innerHTML = 'Nome incorreto'
+      } 
+}; */
 
-            let hp = habilidades[0].base_stat;
-            let attack = habilidades[1].base_stat;
-            let defense = habilidades[2].base_stat;
-            let specialAttack = habilidades[3].base_stat;
-            let specialDefense = habilidades[4].base_stat;
-            let speed = habilidades[5].base_stat; 
+const conectarAPI = async () => {
 
-            console.log(hp)
-            console.log(habilidades[i])
-      } */
-      
+      // buscando o valor do HTML - pelo nome:
+      let nomePokemon = document.querySelector("#nomePokemon").value.toLowerCase();
+      let idPokemon = document.querySelector("#idPokemon").value;
 
-// API que mostra a imagem:
-const urlImagem = `https://pokeres.bastionbot.org/images/pokemon/${idPokemon}.png`;
 
-// mostrar na página:
-document.querySelector("#mostrarImagem").src = urlImagem;
+      // se ambos campos estiverem vazios:
+      if (nomePokemon == "" && idPokemon == "") {
+           return document.querySelector("#erro").innerHTML = 'Preencha um dos campos';
+      }
+
+      // buscar pokemon pelo id:
+      if (nomePokemon == "") {
+            var url = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
+      }
+      //buscar pokemon pelo nome:
+      if (idPokemon == '') {
+            var url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
+            // verificarNomePokemon(nomePokemonAPI);
+      }
+
+      var dados = await fetch(url);
+      var infoNecessaria = await dados.json();
+      var idAPI = infoNecessaria.id;
+
+      habilidadesPokemon(infoNecessaria);
+
+       
+      // API que mostra a imagem:
+      const urlImagem = `https://pokeres.bastionbot.org/images/pokemon/${idAPI}.png`;
+
+      // mostrar a imagem na página:
+      document.querySelector("#mostrarImagem").src = urlImagem;
+
+      // mostrar as habilidades na página:
+      document.querySelector("#divHabilidades").style.display = "flex";
 };
 
 document.querySelector("#mostrar").addEventListener("click", conectarAPI);
+
+
+// Limpando o HTML:
+const limparPesquisa = () =>{
+      document.querySelector("#nomePokemon").value = ''
+      document.querySelector("#idPokemon").value = ''
+      document.querySelector("#divHabilidades").style.display = 'none'
+}
+document.querySelector("#limpar").addEventListener("click", limparPesquisa);
