613c9d4f6f1bfab006f15c7c20e1c144550ebe85
Thaisllayne <thaisllaynesantana@gmail.com>
2021-07-14 17:37:32 -0300

    Refatorando HabilidadesPokemon

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 const habilidadesPokemon = (infoNecessaria) =>{
+
       // Habilidades do Pokémon:
-      var habilidades = infoNecessaria.stats;
-
-      let hp = habilidades[0].base_stat;
-      let attack = habilidades[1].base_stat;
-      let defense = habilidades[2].base_stat;
-      let specialAttack = habilidades[3].base_stat;
-      let specialDefense = habilidades[4].base_stat;
-      let speed = habilidades[5].base_stat; 
-
-      document.querySelector("#hp").innerHTML = hp;
-      document.querySelector("#attack").innerHTML = attack;
-      document.querySelector("#defense").innerHTML = defense;
-      document.querySelector("#specialAttack").innerHTML = specialAttack;
-      document.querySelector("#specialDefense").innerHTML = specialDefense;
-      document.querySelector("#speed").innerHTML = speed;      
+      for (let i = 0; i <= 5; i++) {
+            NomeHabilidades = infoNecessaria.stats[i].stat.name;
+            ValorHabilidades = infoNecessaria.stats[i].base_stat;  
+            
+            let tr = document.createElement("tr");
+            tr.id = NomeHabilidades;
+            tr.innerHTML = NomeHabilidades + ": " + ValorHabilidades
+            document.querySelector("#habilidadesPersonagens").appendChild(tr);
+      }    
 }
 
 /*  const verificarNomePokemon = async (nomePokemonAPI) => {
@@ -32,7 +27,6 @@ const conectarAPI = async () => {
       // buscando o valor do HTML - pelo nome:
       let nomePokemon = document.querySelector("#nomePokemon").value.toLowerCase();
       let idPokemon = document.querySelector("#idPokemon").value;
-
 
       // se ambos campos estiverem vazios:
       if (nomePokemon == "" && idPokemon == "") {
@@ -52,6 +46,8 @@ const conectarAPI = async () => {
       var dados = await fetch(url);
       var infoNecessaria = await dados.json();
       var idAPI = infoNecessaria.id;
+
+      console.log(infoNecessaria);  
 
       habilidadesPokemon(infoNecessaria);
 
